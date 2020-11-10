cc978f7b4c63ec0a4c68f5a255e10ef6a4fc2374
tanner.dailey <daileytanner@gmail.com>
2020-11-10 14:01:49 -0700

    navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import {withRouter, Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Navbar, Button} from 'react-bootstrap';
 
 class Nav extends Component {
     render() {
         return (
-            <p>Nav</p>
-        )
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link to='/map'>Features</Link>
+                    <Link to='/user'>Pricing</Link>
+                    <Button>Logout</Button>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+                    )
     }
 }
 
-export default Nav;
+const mapStateToProps = reduxState => reduxState;
+
+export default connect(mapStateToProps)(withRouter(Nav))
