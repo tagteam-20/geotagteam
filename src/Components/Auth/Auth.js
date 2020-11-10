5f1153cf288e9a2fe29c19181df3e839595fc6bd
Mac <rhettced@gmail.com>
2020-11-10 11:35:09 -0700

    worked with auth page

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
-
+import Axios from 'axios';
 
 class Auth extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    //function registers user
+    registerUser = () =>{
+        const {username,password} = this.state;
+        Axios.post('/api/register',{username,password})
+        .then(res =>{
+            this.props.getUser(res.data)
+            this.props.history.push('/map')
+        })
+    }
+
+    //function updates state of input boxes on change
+    handleInput = (event)  => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
     render() {
+        console.log(this.state);
         return (
-            <p>Auth</p>
+            <div>
+                <input name='username' placeholder='username' onChange={this.handleInput}/>
+                <input name='password'placeholder='password'onChange={this.handleInput}/> 
+            </div>
         )
     }
 }
