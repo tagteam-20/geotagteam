c2932e8bc53f0422094b3fd304ae7c2452626c4e
ermalvido <ermalvido@gmail.com>
2020-11-12 12:09:30 -0700

    started gem page

--- src/Components/Gem/Gem.js
+++ src/Components/Gem/Gem.js
@@ -1,6 +1,16 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Gem extends Component {
+    componentDidMount() {
+        axios
+        .get('/api/pin/' + this.props.match.params.id)
+        .then(res => {
+            console.log(res.data)
+        })
+    }
+
     render() {
         return (
             <p>Gem</p>
